11c29da4b0931e27ce2eda0aad29f56b0e5d0216
fmacedosantos <felipemacedo986@gmail.com>
2024-08-14 23:40:40 -0300

    Implementando a interface

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const handleAddTask = () => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inline}>
+        <TextInput style={styles.enter}/>
+        <Button title='ADICIONAR TAREFA' onPress={handleAddTask}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +19,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4939ba',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inline: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '50%',
+  },
+  enter: {
+    borderColor: 'white',
+    borderWidth: 1,
+    backgroundColor: '#5450d6',
+    width: '80%',
+    color: 'white',
+    marginRight: 5
+  }
 });
